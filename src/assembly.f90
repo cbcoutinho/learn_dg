241f208662e6e50ea4218f48eced05c129b7b173
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-01-22 06:56:13 +0100

    Add some comments and formatting

--- src/assembly.f90
+++ src/assembly.f90
@@ -10,12 +10,17 @@ contains
   subroutine initialize_global_mats(num_nodes, &
                                   & GlobalA, &
                                   & GlobalB, &
-                                  & GlobalX, &
-                                  & xcoords)
+                                  & GlobalX)
+    !+ This routine initalizes the global stiffness matrix, global rhs vector,
+    !+ and global solution vector based on the number of nodes in the system
     integer,  intent(in)                                :: num_nodes
-    real(wp), intent(out),  dimension(:),   allocatable :: GlobalB, GlobalX
+      !+ The number of nodes in the system
+    real(wp), intent(out),  dimension(:),   allocatable :: GlobalB
+      !+ Global RHS Vector
+    real(wp), intent(out),  dimension(:),   allocatable :: GlobalX
+      !+ Global solution vector
     real(wp), intent(out),  dimension(:,:), allocatable :: GlobalA
-    real(wp), intent(in),   dimension(:)                :: xcoords
+      !+ Global mass matrix
 
     allocate(GlobalA(num_nodes, num_nodes))
     allocate(GlobalB(num_nodes))
@@ -24,16 +29,25 @@ contains
     ! Intial Conditions
     GlobalA(:,:)  = 0._wp
     GlobalX(:)    = 0._wp
+    GlobalB(:)    = 0._wp
 
     return
   end subroutine initialize_global_mats
 
   subroutine assemble(order, xcoords, elem_conn, GlobalA, diff, vel)
+    !+ Assemble the global stiffness matrix based on element connectivity
     integer,  intent(in),   dimension(:)    :: order
+      !+ Order of element(s)
     integer,  intent(in),   dimension(:,:)  :: elem_conn
-    real(wp), intent(in)                    :: diff, vel
+      !+ Element connectivity
+    real(wp), intent(in)                    :: diff
+      !+ Diffusivity coefficient [m/s^2]
+    real(wp), intent(in)                    :: vel
+      !+ Velocity [m/s]
     real(wp), intent(in),   dimension(:)    :: xcoords
+      !+ Array of nodal coordinates
     real(wp), intent(out),  dimension(:,:)  :: GlobalA
+      !+ Global Stiffness matrix
 
     integer :: ii
     real(wp), dimension(:,:), allocatable   :: Ie
@@ -65,28 +79,31 @@ contains
   end subroutine assemble
 
   subroutine set_BCs(xcoords, GlobalB, GlobalA)
+    !+ Set boundary conditions in GlobalA and GlobalB using two Dirchlet
+    !+ boundaries
     real(wp), intent(in),   dimension(:)    :: xcoords
+      !+ Array of nodal coordinates
     real(wp), intent(out),  dimension(:)    :: GlobalB
+      !+ Global RHS Vector
     real(wp), intent(out),  dimension(:,:)  :: GlobalA
-
+      !+ Global Mass Matrix
     integer,  dimension(1)  :: iloc
-
-    iloc = minloc(xcoords)
-    GlobalA(iloc,:) = 0._wp
-    GlobalA(iloc,iloc) = 1._wp
-
-    iloc = maxloc(xcoords)
-    GlobalA(iloc,:) = 0._wp
-    GlobalA(iloc,iloc) = 1._wp
+      !+ Index variable to locate node numbers based on xcoords
 
     ! Left Boundary Dirchlet BC
-    GlobalB(:)    = 0._wp
+    iloc = minloc(xcoords)
+    GlobalA(iloc,:)     = 0._wp
+    GlobalA(iloc,iloc)  = 1._wp
+    GlobalB(iloc)       = 0._wp
+
     ! Right Boundary Dirchlet BC
-    GlobalB(maxloc(xcoords))    = 1._wp
+    iloc = maxloc(xcoords)
+    GlobalA(iloc,:)     = 0._wp
+    GlobalA(iloc,iloc)  = 1._wp
+    GlobalB(iloc)       = 1._wp
 
     ! call r8mat_print(num_nodes, 1, GlobalB, 'Global RHS:')
     ! stop
-
 
     return
   end subroutine set_BCs
