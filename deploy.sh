f8f5dfde9fb6de476e29f27a76531225662f50bd
cbcoutinho <chrisbcoutinho@gmail.com>
2017-02-03 23:19:47 +0100

    Update email options
    Turn on packages again
    
    
    No other packages besides gfortran
    
    
    Switch back to precise
    
    
    Switch back to python3, state group and os
    
    
    Use trusty
    
    
    No cache
    
    
    Use python 2.7, maybe?
    
    
    No more stable
    
    
    Maybe binutils is needed
    
    
    Remove sudo again
    
    
    Need to say which version python
    
    
    Reintroduce trusty and sudo
    
    
    Get rid of sudo
    
    
    Switch back to precise, change some installs
    
    
    Turn on sudo

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,14 @@ if [ ! "$TRAVIS" ]; then
     exit 0
 fi
 
+if [ ! "$TRAVIS_BRANCH" != "master" ]; then
+  echo "Skipping documentation deployment"
+  echo "Only applicable to master branch"
+  exit 0
+
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     echo "Skipping documentation deployment"
+    echo "Not applicable to pull requests"
     exit 0
 fi
 
