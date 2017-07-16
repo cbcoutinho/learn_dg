ae3b55998975333cc7496a8ccd2a761e0d55e2f7
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-07-16 21:43:05 +0200

    Rename some loops, replace some slices with pointers

--- src/assembly/smod_assemble_2D.f90
+++ src/assembly/smod_assemble_2D.f90
@@ -22,15 +22,15 @@ contains
     real(wp), dimension(N,N)              :: Ie
 
     ! Local variables
-    integer                               :: node1, node2
-    real(wp), dimension(:,:), allocatable :: alpha
+    integer                           :: node1, node2
+    real(wp), dimension(N,N), target  :: alpha
 
     ! Get the coefficients of the basis functions (alpha)
     ! Supported 2D basis functions:
     !   Bi-linear quadrilaterals (N=4)
     !   Bi-quadratic quadrilaterals (N=9)
     !   Bi-cubic quadrilaterals (N=16)
-    if ( .not. allocated(alpha) ) alpha = getAlpha(N)
+    alpha = getAlpha(N)
 
     Ie = 0._wp
 
@@ -50,12 +50,13 @@ contains
       real(wp), dimension(:,:), allocatable :: out
 
       ! Local variables
-      integer                   :: ii, jj, num_pts
-      real(wp), parameter       :: eps = epsilon(0e0)
-      real(wp)                  :: fun1, fun2
-      real(wp), dimension(2)    :: dfun1, dfun2
-      real(wp)                  :: detJ
-      real(wp), dimension(2,2)  :: J, invJ
+      integer                         :: ii, jj, num_pts
+      real(wp), parameter             :: eps = epsilon(0e0)
+      real(wp)                        :: fun1, fun2
+      real(wp), dimension(2)          :: dfun1, dfun2
+      real(wp)                        :: detJ
+      real(wp), dimension(2,2)        :: J, invJ
+      real(wp), dimension(:), pointer :: alpha_row
 
       ! Initialize function output. Actual number of pts is num_pts*num_pts,
       ! because the meshgrid goes in both x and y directions. Only need one.
@@ -73,20 +74,22 @@ contains
           invJ  = inv2(J)
           detJ  = det2(J)
 
+          alpha_row => alpha(:,node1)
+
           ! If fun1 is just N_i, use dot_product to determine N_i
           if ( d1 == 0 ) then
-            fun1 = dot_product(alpha(:,node1), getArow(N, xi(ii,jj), eta(ii,jj)))
+            fun1 = dot_product(alpha_row, getArow(N, xi(ii,jj), eta(ii,jj)))
           else
 
             ! If fun1 contains a derivative, need to calc N_i,xi and N_i,eta
             dfun1(1) = ( &
-            dot_product(alpha(:,node1), getArow(N, xi(ii,jj)+eps, eta(ii,jj))) - &
-            dot_product(alpha(:,node1), getArow(N, xi(ii,jj)-eps, eta(ii,jj))) &
+            dot_product(alpha_row, getArow(N, xi(ii,jj)+eps, eta(ii,jj))) - &
+            dot_product(alpha_row, getArow(N, xi(ii,jj)-eps, eta(ii,jj))) &
             ) / ( 2._wp*eps )
 
             dfun1(2) = ( &
-            dot_product(alpha(:,node1), getArow(N, xi(ii,jj), eta(ii,jj)+eps)) - &
-            dot_product(alpha(:,node1), getArow(N, xi(ii,jj), eta(ii,jj)-eps)) &
+            dot_product(alpha_row, getArow(N, xi(ii,jj), eta(ii,jj)+eps)) - &
+            dot_product(alpha_row, getArow(N, xi(ii,jj), eta(ii,jj)-eps)) &
             ) / ( 2._wp*eps )
 
             ! N_i,x = dxi/dx * N_i,xi + deta/dx * N_i,eta
@@ -94,20 +97,22 @@ contains
 
           endif
 
+          alpha_row => alpha(:,node2)
+
           ! If fun2 is just N_i, use dot_product to determine N_i
           if ( d2 == 0 ) then
-            fun2 = dot_product(alpha(:,node2), getArow(N, xi(ii,jj), eta(ii,jj)))
+            fun2 = dot_product(alpha_row, getArow(N, xi(ii,jj), eta(ii,jj)))
           else
 
             ! If fun2 contains a derivative, need to calc N_i,xi and N_i,eta
             dfun2(1) =  ( &
-            dot_product(alpha(:,node2), getArow(N, xi(ii,jj)+eps, eta(ii,jj))) - &
-            dot_product(alpha(:,node2), getArow(N, xi(ii,jj)-eps, eta(ii,jj))) &
+            dot_product(alpha_row, getArow(N, xi(ii,jj)+eps, eta(ii,jj))) - &
+            dot_product(alpha_row, getArow(N, xi(ii,jj)-eps, eta(ii,jj))) &
             ) / ( 2._wp*eps )
 
             dfun2(2) =  ( &
-            dot_product(alpha(:,node2), getArow(N, xi(ii,jj), eta(ii,jj)+eps)) - &
-            dot_product(alpha(:,node2), getArow(N, xi(ii,jj), eta(ii,jj)-eps)) &
+            dot_product(alpha_row, getArow(N, xi(ii,jj), eta(ii,jj)+eps)) - &
+            dot_product(alpha_row, getArow(N, xi(ii,jj), eta(ii,jj)-eps)) &
             ) / ( 2._wp*eps )
 
             ! N_i,y = dxi/dy * N_i,xi + deta/dy * N_i,eta
@@ -144,7 +149,7 @@ contains
     ! NOTE: This is assuming that all elements have the same number of points
     !$OMP PARALLEL DEFAULT(SHARED) PRIVATE(ii, xy, Ie) REDUCTION(+:GlobalA)
     !$OMP DO
-    do ii = 1, num_cells
+    outer: do ii = 1, num_cells
 
       xy = points(cells(ii,:), :)
 
@@ -157,7 +162,7 @@ contains
           & GlobalA(cells(ii,:), cells(ii,:)) - diff*Ie
 
       ! Add elemental matrix for velcity in X (1) and Y (2) to GlobalA
-      do jj = 1, 2
+      adv: do jj = 1, 2
         if ( abs(vel(jj)) .gt. eps ) then
 
         Ie = assembleElementalMatrix(num_pts, 0, jj, xy)
@@ -166,8 +171,8 @@ contains
             & GlobalA(cells(ii,:), cells(ii,:)) - vel(jj)*Ie
 
         endif
-      enddo
-    enddo
+      enddo adv
+    enddo outer
     !$OMP END DO
     !$OMP END PARALLEL
 
